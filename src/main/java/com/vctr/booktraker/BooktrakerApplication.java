cb92cb266063b77c1e4aa89131d49dbb57c28842
victor <victordarocha20@hotmail.com>
2025-04-16 17:19:45 -0300

    first commit

---
+++ src/main/java/com/vctr/booktraker/BooktrakerApplication.java
@@ -0,0 +1,13 @@
+package com.vctr.booktraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vctr/booktraker/BooktrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vctr.booktraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
