6b63855f6374ac2687575af0bf25e652c02d8f6d
victor <victordarocha20@hotmail.com>
2025-04-17 01:17:50 -0300

    added configuration to build dynamoDB locally(docker), component to create table and profile to change local to cloud environment

--- src/main/java/com/vctr/booktraker/config/DynamoDBConfig.java
+++ src/main/java/com/vctr/booktraker/config/DynamoDBConfig.java
@@ -1,17 +1,36 @@
 package com.vctr.booktraker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
 
 @Configuration
 public class DynamoDBConfig {
 
+    @Profile("local")
     @Bean
-    public DynamoDbClient dynamoDbClient() {
+    public DynamoDbClient localDynamoDbClient(
+            @Value("${dynamodb.endpoint}") String endpoint,
+            @Value("${dynamodb.region}") String region
+    ) {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Profile("prod")
+    @Bean
+    public DynamoDbClient awsDynamoDbClient(
+            @Value("${dynamodb.region}") String region
+    ) {
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
                 .build();
     }
 
